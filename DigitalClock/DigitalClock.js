863ed037a5eb80ee7c59400bb5236760499e8c3d
anjani08 <81013997+anjani08@users.noreply.github.com>
2021-03-24 15:21:56 +0530

    Add files via upload

---
+++ DigitalClock/DigitalClock.js
@@ -0,0 +1,44 @@
+function showTime() {
+		var time = new Date();
+		var hour = time.getHours();
+		var min = time.getMinutes();
+		var sec = time.getSeconds();
+		var session = "PM";
+
+		if(hour >= 12){
+			hour -= 12;
+			session = "AM";
+		}
+		if(hour == 0){
+			hour =12;
+		}
+
+		hour = hour < 10 ? "0" + hour : hour;
+		min = min < 10 ? "0" + min : min;
+		sec = sec < 10 ? "0" + sec : sec;
+
+		document.getElementById("time").innerText= hour + " : " + min + " : " + sec + " " +session;
+
+
+}
+showTime();
+
+function showDate(){
+	var date = new Date();
+	var day = date.getDate();
+	var month = date.getMonth()+1;
+	var year = date.getFullYear();
+
+	document.getElementById("date").innerText = day + " / " + month + " / " + year;
+}
+showDate();
+
+function startTime(){
+	setInterval(showTime,1000);
+}
+
+function stopTime()
+{
+	clearInterval(startTime.setInterval);
+}
+
